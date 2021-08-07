72380aa908e8debfc7027c846e7e1f1bef6b513b
Jerrydai90 <Jerrydai90@gmail.com>
2021-08-07 10:36:31 +0800

    111

--- b2-f1.java
+++
@@ -1,6 +0,0 @@
-NODE_ENV=development
-VUE_APP_API_BASE_URL=http://192.168.4.181:8202/
-VUE_APP_CAS_BASE_URL=http://cas.example.org:8443/cas
-VUE_APP_ONLINE_BASE_URL=http://fileview.jeecg.com/onlinePreview
-VUE_APP_PROJECT_NAME=ssfsf
-VUE_APP_PROJECT_LOGINBG=001.png
